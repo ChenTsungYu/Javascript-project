c364c7c18181a2336994c1efbb8f8d2d4b428fdd
Tsung yu <z3337059@gmail.com>
2019-05-24 17:30:50 +0800

    Update switch

--- setup-agency/js/app.js
+++ setup-agency/js/app.js
@@ -1,5 +1,27 @@
 $(document).ready(function () {
-
+    let isPlay = false; // state of video switch
+    // preloader
     $('.preloader').fadeOut();
+
+    // navBtn
+    $('.navBtn').on('click', () => {
+        $('.nav').toggleClass('nav--show');
+    });
+ // video switch  
+ $('.video__switch-container').on('click',  ()=> {
+      // 檢查第一個  video__switch-btn 元素是否包含 
+   //  const value = $('.video__switch-btn').hasClass('btnSlide');
+   isPlay = !isPlay;
+    if (isPlay) {
+        $('.video__switch-btn').removeClass('btnSlide');
+ // 獲得第一個元素的名稱和值，play() 方法開始播放當前的影片
+        $('#video').get(0).play();
+    } else {
+        $('.video__switch-btn').add('btnSlide');
+        // 獲得第一個元素的名稱和值，play() 方法開始播放當前的影片
+               $('#video').get(0).play();
+    }
+
+ });
 
 })
