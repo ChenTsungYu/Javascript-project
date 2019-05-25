12215ee51de8eefb636c66d8ded7afa0fd4db262
Tsung yu <z3337059@gmail.com>
2019-05-25 19:37:34 +0800

    Update about

--- setup-agency/js/app.js
+++ setup-agency/js/app.js
@@ -1,17 +1,18 @@
 $(document).ready(function () {
-    let isPlay = false; // state of video switch
+    let isPlay = true; // state of video switch
     // preloader
     $('.preloader').fadeOut();
 
     // navBtn
     $('.navBtn').on('click', () => {
-        $('.nav').toggleClass('nav--show');
+        $('.navBtn').toggleClass('navBtn--action')
+        $('.nav').toggleClass('nav--show')
     });
  // video switch  
  $('.video__switch-container').on('click',  ()=> {
-      // 檢查第一個  video__switch-btn 元素是否包含 
+      // 檢查第一個  video__switch-btn 元素是否包含 btnSlide的class
    //  const value = $('.video__switch-btn').hasClass('btnSlide');
-   isPlay = !isPlay;
+   isPlay = !isPlay; 
     if (isPlay) {
         $('.video__switch-btn').removeClass('btnSlide');
  // 獲得第一個元素的名稱和值，play() 方法開始播放當前的影片
@@ -19,9 +20,22 @@ $(document).ready(function () {
     } else {
         $('.video__switch-btn').add('btnSlide');
         // 獲得第一個元素的名稱和值，play() 方法開始播放當前的影片
-               $('#video').get(0).play();
+               $('#video').get(0).pause();
     }
-
  });
+ // 從magnific.js 找code貼上
+ // magnific popup 音箱效果
+ $('#projects__modal').magnificPopup({
+    delegate: 'a', // child items selector, by clicking on it popup will open
+    type: 'image',
+    gallery: {
+     // options for gallery
+     enabled: true
+    }
+    // other options
+   });
+
+
+
 
 })
